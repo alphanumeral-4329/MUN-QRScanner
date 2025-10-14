89978ccfd79189e4a0169ae1200419c4c422c3a8
thundanebpulsar_2342 <nirolaraghav27@gmail.com>
2025-10-15 01:16:03 +0530

    Update B2_main.py
    more indent fixing

--- B2_main.py
+++ B2_main.py
@@ -219,11 +219,11 @@ def debug_redis():
     except Exception as e:
         return f"Error accessing Redis: {e}", 500
         @app.route("/flush_cache")
-    def flush_cache_route():
-    if "oc_id" not in session:
+        def flush_cache_route():
+        if "oc_id" not in session:
         return "Not logged in", 401
-    redis_client.delete("attendance_cache")
-    return "✅ attendance_cache cleared"
+        redis_client.delete("attendance_cache")
+        return "✅ attendance_cache cleared"
 
 
 if __name__=="__main__":
